6d7e12f1a8c366cc1826d790ebe01cd1853a11ba
csj <chris.stjohn@gmail.com>
2019-03-06 20:46:56 -0500

    SHELL => CHOPPED_DOUGH Tooltips added when a new round starts

--- src/main/kotlin/com/codingame/game/model/Constants.kt
+++ src/main/kotlin/com/codingame/game/model/Constants.kt
@@ -2,7 +2,7 @@ package com.codingame.game.model
 
 object Constants {
   enum class ITEM {
-    FOOD, DOUGH, DISH, STRAWBERRIES, SHELL, RAW_TART
+    FOOD, DOUGH, DISH, STRAWBERRIES, CHOPPED_DOUGH, RAW_TART
   }
   
   enum class FOOD {
